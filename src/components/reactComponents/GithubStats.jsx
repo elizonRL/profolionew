b956a1a5771bba7f3653b4b92c85e1946fd71eb8
Elizon Rodrigues Liriano <118481749+elizonRL@users.noreply.github.com>
2025-07-10 08:55:03 -0400

    change: the github stats change

--- src/components/reactComponents/GithubStats.jsx
+++ src/components/reactComponents/GithubStats.jsx
@@ -1,25 +1,80 @@
-import TitleHeader from "../TitleHeader.astro";
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 const GithubStats = () => {
   // Ensure the USER_NAME environment variable is set
-  const username = import.meta.env.USER_NAME;
+  const username = import.meta.env.USER_NAME || 'elizonRL'; // Default to 'elizonRL' if not set
+  const githubUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
   const [repos, setRepos] = useState([]);
   useEffect(() => {
     if (username) {
-      fetch(`https://api.github.com/users/${username}/repos`)
+      fetch(githubUrl)
         .then((response) => response.json())
-        .then((data) => setRepos(data))
-        .catch((error) => console.error("Error fetching GitHub repos:", error));
+        .then((data) =>
+          setRepos(
+            data.map((repo) => ({
+              id: repo.id,
+              name: repo.name,
+              description: repo.description || 'No description available',
+              html_url: repo.html_url,
+              stars: repo.stargazers_count,
+              forks_count: repo.forks_count,
+            }))
+          ).catch((error) =>
+            console.error('Error fetching GitHub repos:', error)
+          )
+        );
     } else {
-      console.error("GitHub username is not defined.");
+      console.error('GitHub username is not defined.');
     }
   }, []);
-  console.log("GitHub Repos:", repos);
+  console.log(
+    'GitHub Repos:',
+    repos.sort((a, b) => b.stars - a.stars).slice(0, 6)
+  );
   return (
     <>
-      <div className="grid grid-cols-6 grid-rows-6 gap-4 w-full max-w-6xl mx-auto">
-        <div className="col-span-3 row-span-3">
+      <div className='grid grid-cols-5 grid-rows-5 gap-5 p-4 justify-center items-start'>
+        <div className='col-span-2 row-span-5 bg-slate-800 hover:bg-slate-700 border-gray-300 pr-4 rounded-xl shadow-lg'>
+          <h2 className='text-xl font-semibold mb-4 pt-2 text-center text-white'> <svg width={26} height={26} className='inline mr-2'>
+            <use href='/assets/sprite.svg#github' />
+            </svg> Repositorios de {username}</h2>
+         <ul className='list-none p-3 text-sm'> 
+            {repos.map((repo => (
+              <li key={repo.id} className='mb-2 '>
+                <a
+                  href={repo.html_url}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='text-blue-500 hover:underline font-semibold text-xl'
+                >
+                  {repo.name}
+                </a>
+                <p className='text-gray-600'>{repo.description}</p>
+                <span className='text-sm text-gray-500'>
+                  ⭐ {repo.stars} | 🍴 {repo.forks_count}
+                </span>
+              </li>
+            ))).sort((a, b) => b.stars - a.stars).slice(0, 6)}
+          </ul>
+        </div>
+        <div className='col-span-2 row-span-2 col-start-3'>
+          <img
+            src={`https://github-readme-stats.vercel.app/api?username=${username}&show_icons=true&theme=radical`}
+            alt='GitHub Stats'
+            loading='lazy'
+            className='w-full h-full object-contain rounded-xl shadow-lg aspect-ratio'
+          />
+        </div>
+        <div className='col-span-2 row-span-2 col-start-3 row-start-4'>
+          <img
+            src={`https://github-readme-stats.vercel.app/api/top-langs/?username=${username}&layout=compact&theme=radical`}
+            alt='Lenguajes más usados'  
+            loading='lazy'
+            className='w-full h-full object-contain rounded-xl shadow-lg aspect-ratio'
+          />
+        </div>
+
+        {/* <div className="col-span-3 row-span-3">
           <img
             src={`https://github-readme-stats.vercel.app/api?username=${username}&show_icons=true&theme=radical`}
             alt="GitHub Stats"
@@ -62,7 +117,7 @@ const GithubStats = () => {
             </div>
           </div>
          )).slice(0, 6)}
-        </div>
+        </div> */}
       </div>
     </>
   );
